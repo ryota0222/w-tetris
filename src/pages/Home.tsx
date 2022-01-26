71652be9fc2b79fd499aacba6dc9e327fd57281e
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-01-26 21:34:39 +0900

    トップページの実装

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,39 @@
-import { useDarkMode } from '@/hooks/useDarkMode'
-import PlayScreen from '@/components/PlayBoard'
-import { useTetrisContext } from '@/contexts/TetrisProvider'
-import { useMemo, useRef } from 'react'
-import { GameTheme } from '@/types'
+import PlayBoard from '@/components/PlayBoard'
+import ScoreBoard from '@/components/ScoreBoard'
+import NextBoard from '@/components/NextBoard'
+import css from '@/styles/home.module.css'
+import DescriptionBoard from '@/components/DescriptionBoard'
+import OperationBoard from '@/components/OperationBoard'
+import Spacer from '@/components/Spacer'
 
 const Home = () => {
-  const { start, pause, restart, end, mode } = useTetrisContext()
-  const refStartButton = useRef<null | HTMLButtonElement>(null)
   return (
-    <div>
-      <PlayScreen />
-      <button
-        onClick={() => {
-          start(`${mode}-green` as GameTheme)
-          if (refStartButton.current) {
-            refStartButton.current.blur()
-          }
-        }}
-        className="mr-4 text-white"
-        ref={refStartButton}
+    <div className="flex flex-col h-full">
+      <Spacer />
+      <div
+        className={`grid grid-cols-5 grid-row-4 sm:grid-cols-5 sm:grid-rows-11 ${css.container}`}
       >
-        start
-      </button>
-      <button onClick={pause} className="mr-4 text-white">
-        pause
-      </button>
-      <button onClick={restart} className="mr-4 text-white">
-        restart
-      </button>
-      <button onClick={end}>end</button>
+        <div className="row-start-1 row-span-4 col-start-1 col-end-3 sm:col-end-4 sm:row-end-9">
+          <div className="flex box-border px-2">
+            <PlayBoard />
+          </div>
+        </div>
+        <div className="row-start-1 row-span-3 col-start-3 col-end-6 sm:col-start-4 sm:col-end-6 sm:row-span-8 pl-4 sm:pl-4 sm:mr-2">
+          <div
+            className={`pl-8 sm:pl-0 flex sm:flex-col justify-between h-full pb-4 sm:pb-2 ${css.panels}`}
+          >
+            <div className="space-y-4">
+              <ScoreBoard />
+              <NextBoard />
+            </div>
+            <DescriptionBoard />
+          </div>
+        </div>
+        <div className="row-start-4 col-start-3 col-end-6 sm:col-span-full sm:row-start-9 sm:row-end-12 pl-8 sm:pl-0">
+          <OperationBoard />
+        </div>
+      </div>
+      <Spacer />
     </div>
   )
 }
